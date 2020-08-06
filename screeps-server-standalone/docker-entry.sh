40a9611286faf0f56f23f10bdcf23d9bf608b9bd
Geoff Potter <geoffpot@gmail.com>
2020-08-05 20:46:03 -0400

    add stand alone version from Ags code to see if that even works

--- screeps-server-standalone/docker-entry.sh
+++ screeps-server-standalone/docker-entry.sh
@@ -3,28 +3,27 @@ set -e
 
 function init_srv(){
 	echo "===== SETTING UP SCREEPS ====="
-	#setup directories
-    mkdir -p /server
-    chown -R nobody:users /server
-    #install screeps
-    cd /server
-    npm install screeps
-
+	yarn init -y
+	yarn add screeps
+	npx screeps init
 	#install standard mods
 	npm install screepsmod-mongo screepsmod-auth screepsmod-tickrate screepsmod-admin-utils screepsmod-features
 }
 
-function run_srv(){
-	#run screeps
-    cd /server
-    exec npx screeps start
-}
 
+function run_srv(){
+	echo "===== RUNNING SCREEPS ====="
+	cd /server
+	npx screeps start
+}
 case $1 in
 	init)
 		init_srv
 		;;
-	*)
+	run)
 		run_srv
+		;;
+	*)
+		exec "$@"
 		;;
 esac
