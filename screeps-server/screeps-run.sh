cca4abdeea0b7be6b25007d25f884086b64201c9
Geoff Potter <geoffpot@gmail.com>
2020-08-12 17:33:09 -0400

    copy run script changes over to standalone version

--- screeps-server/screeps-run.sh
+++ screeps-server/screeps-run.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -e
 echo "STARTING SERVER...."
-SETTINGS_FILE=/server/.screepssc
+SETTINGS_FILE=/server/.screepsrc
 
 cd /server
-echo test -f ".screepssc"
-ls -a
+
 if [ ! -f "$SETTINGS_FILE" ] 
 then
     echo "===== SETTINGS FILE MISSING, SETTING UP SCREEPS ====="
     yarn init -y
     yarn add screeps@"$SCREEPS_VERSION"
     echo "$API_KEY" | npx screeps init
-    #install standard mods
-    #npm install screepsmod-mongo screepsmod-auth screepsmod-tickrate screepsmod-admin-utils screepsmod-features
+    #install mods
     npm install $PLUGINS
 fi
 
